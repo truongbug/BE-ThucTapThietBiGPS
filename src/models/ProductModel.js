681a8f9e9affcbde32ca648234631caf0d9bc1c6
truongbug <truonggit1903@gmail.com>
2024-07-18 07:38:41 +0700

    project GPS

---
+++ src/models/ProductModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true, unique: true },
+        image: { type: String, required: true },
+        type: { type: String, required: true },
+        price: { type: Number, required: true },
+        countInStock: { type: Number, required: true },
+        rating: { type: Number, required: true },
+        description: { type: String },
+        discount: { type: Number },
+        selled: { type: Number },
+        videoUrl: {type: String , require: true}
+    },
+    {
+        timestamps: true,
+    }
+);
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
